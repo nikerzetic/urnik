faa46bb7b87a467606fe3de6b274c2fc7bb8d833
Nik Erzetic <nikerzetic@gmail.com>
2019-05-16 18:13:28 +0200

    Dodal pretvarjanje teden formata v datetime tuple

--- urnik/utils.py
+++ urnik/utils.py
@@ -5,3 +5,7 @@ def teden_dneva(dan):
     ponedeljek = dan - datetime.timedelta(days=dan.weekday())
     nedelja = ponedeljek + datetime.timedelta(days=6)
     return (ponedeljek, nedelja)
+
+def teden_int_v_datetime(teden):
+    teden = datetime.datetime.strptime(teden, "%Y-%m-%d")
+    return teden_dneva(teden)
